f8f306a4c2819c3bbc18d0e39c8bef6b5421958c
apdonaghy <adonaghy@seattleu.edu>
2020-08-20 17:22:05 -0700

    added google analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import VueAnalytics from 'vue-analytics'
 import {library} from "@fortawesome/fontawesome-svg-core";
 
 import {
@@ -11,6 +11,13 @@ import {
 library.add(faTrash, faLink, faListUl,faRandom, faUndo, faStar, faEnvelope, faSpinner, faTimes, faUpload);
 
 Vue.config.productionTip = false;
+
+// Google Analytics id
+Vue.use(VueAnalytics, {
+  id: 'UA-174260847-1',
+  router
+})
+
 
 new Vue({
   router,
